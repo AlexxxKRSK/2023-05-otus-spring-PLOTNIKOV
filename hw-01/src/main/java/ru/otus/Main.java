ae3f2bd393cc5752abb46cdc5df731a5d76d153f
Plotnikov Alexander <aleksandr.plotnikov@codemark.ru>
2023-06-26 21:34:28 +0700

    hw-01 fix

--- hw-01/src/main/java/ru/otus/Main.java
+++ hw-01/src/main/java/ru/otus/Main.java
@@ -1,18 +1,12 @@
 package ru.otus;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.domain.Question;
-import ru.otus.service.PrintService;
-import ru.otus.service.QuestionLoaderService;
-
-import java.util.List;
+import ru.otus.service.QuestionService;
 
 public class Main {
     public static void main(String[] args) {
         var ctx = new ClassPathXmlApplicationContext("spring-context.xml");
-        QuestionLoaderService loaderService = ctx.getBean(QuestionLoaderService.class);
-        PrintService printService = ctx.getBean(PrintService.class);
-        List<Question> questions = loaderService.loadQuestions();
-        printService.printQuestions(questions);
+        QuestionService questionService = ctx.getBean(QuestionService.class);
+        questionService.printQuestions();
     }
 }
--- hw-01/src/main/java/ru/otus/service/QuestionServiceImpl.java
+++ hw-01/src/main/java/ru/otus/service/QuestionServiceImpl.java
@@ -1,12 +1,18 @@
 package ru.otus.service;
 
+import lombok.RequiredArgsConstructor;
+import ru.otus.dao.QuestionDao;
 import ru.otus.domain.Question;
 
 import java.util.List;
 
-public class ConsolePrintService implements PrintService {
+@RequiredArgsConstructor
+public class QuestionServiceImpl implements QuestionService {
+    private final QuestionDao questionDao;
+
     @Override
-    public void printQuestions(List<Question> questionList) {
+    public void printQuestions() {
+        List<Question> questionList = questionDao.getQuestions();
         for (var question : questionList) {
             System.out.println(question.getValue());
             for (var answer : question.getAnswers()) {
