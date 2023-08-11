9564511a43dc9ce573eba3f140f17f366a542b90
Plotnikov Alexander <aero2006@mail.ru>
2023-08-11 14:57:35 +0700

    hw-07 review fix

--- hw-07/src/main/java/ru/otus/repository/BookRepository.java
+++ hw-07/src/main/java/ru/otus/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package ru.otus.repository;
 
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.domain.Book;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
+    @EntityGraph(attributePaths = {"genre", "author"})
     List<Book> findAll();
 
     Optional<Book> getBookById(Long id);
--- hw-07/src/test/java/ru/otus/service/impl/CommentServiceImplTest.java
+++ hw-07/src/test/java/ru/otus/service/impl/CommentServiceImplTest.java
@@ -1,25 +1,29 @@
 package ru.otus.service.impl;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.domain.Comment;
 import ru.otus.repository.BookRepository;
+import ru.otus.repository.CommentRepository;
 import ru.otus.service.CommentService;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static ru.otus.BookProvider.getCommentDto;
 import static ru.otus.BookProvider.getExistingBook;
 
 @SpringBootTest
 class CommentServiceImplTest {
 
-    @Mock
+    @MockBean
     private BookRepository bookRepository;
+
+    @MockBean
+    private CommentRepository commentRepository;
 
     @Autowired
     private CommentService commentService;
@@ -27,10 +31,10 @@ class CommentServiceImplTest {
     @Test
     void addComment() {
         var testBook = getExistingBook();
-        var expectedResult = getCommentDto();
+        var commentText = "TEST COMMENT";
         when(bookRepository.getBookById(any())).thenReturn(Optional.of(testBook));
-        var result = commentService.addComment(testBook.getId(),
-                expectedResult.getText());
-        assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
+        when(commentRepository.save(any())).thenReturn(new Comment(commentText));
+        var result = commentService.addComment(testBook.getId(), commentText);
+        assertEquals(result.getText(), commentText);
     }
 }
